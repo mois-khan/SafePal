9b3091ab90de2dc086230761ac0ccb6438638820
Mois Khan <moiskhanmd9090@gmail.com>
2026-03-05 00:26:31 +0530

    Implmented feature 2: One-Tap Report to Cyber Police

--- callshield_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ callshield_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,16 @@ import audioplayers_darwin
 import device_info_plus
 import flutter_local_notifications
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
